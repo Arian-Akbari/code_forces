e671acb1a503caa84461381aa521e880ac704f44
“Arian” <arian09337332108@gmail.com>
2023-10-31 23:58:50 +0330

    answered a new question

--- A_Sorting_with_Twos.cpp
+++ A_Sorting_with_Twos.cpp
@@ -1,42 +1,27 @@
 #include <iostream>
+
 using namespace std;
-int main()
-{
-    int n;
-    cin >> n;
-    int numbers[n];
-    for (int i = 0; i < n; i++)
-        cin >> numbers[i];
-    bool flag = false;
-    // 1 , 2 , 4 , 8, 16
-    for (int i = n - 2; i >= 0; i--)
-    {
-        if (flag)
-            break;
-        if (numbers[i] > numbers[i + 1])
-        {
-            if (i == 7 || i == 15 || i == 3 || i == 1 || i == 0)
-            {
-                int dif = numbers[i] - numbers[i + 1];
-                for (int k = 0; k <= i; k++)
-                {
-                    numbers[k] -= dif;
-                    if (numbers[k] < 0)
-                    {
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-            else
-            {
+
+int main() {
+    int t;
+    cin >> t;
+    while (t != 0) {
+        int n;
+        cin >> n;
+        int numbers[n];
+        for (int i = 0; i < n; i++)
+            cin >> numbers[i];
+        bool flag = false;
+        for (int i = n - 2; i >= 0; i--) {
+            if (numbers[i] > numbers[i + 1] && !(i == 7 || i == 15 || i == 3 || i == 1 || i == 0)) {
                 flag = true;
                 break;
             }
         }
+        if (!flag)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+        t--;
     }
-    if (!flag)
-        cout << "YES" << endl;
-    else
-        cout << "NO" << endl;
 }
